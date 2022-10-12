b084ec6641bc4c489fc04d322ca132020c45e9b7
Gediminas <gedas.fs@gmail.com>
2022-10-12 19:49:38 +0300

    add models

---
+++ src/app/Models/PaymentType.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PaymentType extends Model
+{
+    use HasFactory;
+
+    public function userPaymentTypes()
+    {
+        return $this->hasMany(PaymentType::class);
+    }
+}
