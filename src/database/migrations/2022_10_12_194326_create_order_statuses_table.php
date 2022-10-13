4e3d53e37be3cde24d8490fdf6e721b443fd8701
Gediminas <gedas.fs@gmail.com>
2022-10-13 15:23:51 +0300

    add timestamps cols to '_statuses' tables

--- src/database/migrations/2022_10_12_194326_create_order_statuses_table.php
+++ src/database/migrations/2022_10_12_194326_create_order_statuses_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('order_statuses', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            // $table->timestamps();
+            $table->timestamps();
         });
     }
 
