687913a3bf263b0431dd0c65dc1bab9f08c79631
Gediminas <gedas.fs@gmail.com>
2022-10-13 14:19:39 +0300

    change migration file name (type error in name)

--- src/database/migrations/2022_10_12_194326_create_order_statuses_table.php
+++ src/database/migrations/2022_10_12_194326_create_order_statuses_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('order_statuses_tables', function (Blueprint $table) {
+        Schema::create('order_statuses', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             // $table->timestamps();
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('order_statuses_tables');
+        Schema::dropIfExists('order_statuses');
     }
 };
