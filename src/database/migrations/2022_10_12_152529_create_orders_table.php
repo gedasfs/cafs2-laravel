aba88a9e516dd3b65e548e64ea040ebc3a77d309
Gediminas <gedas.fs@gmail.com>
2022-10-17 18:23:50 +0300

    transfer 'shipping_type' from orders table to separate table

--- src/database/migrations/2022_10_12_152529_create_orders_table.php
+++ src/database/migrations/2022_10_12_152529_create_orders_table.php
@@ -20,8 +20,6 @@ return new class extends Migration
             $table->foreignId('user_payment_type_id')->nullable()->constrained('user_payment_types')->onDelete('cascade');
             $table->string('number');
             $table->string('status')->default('Created');
-            $table->string('shipping_type')->nullable();
-            $table->float('shipping_cost', 6, 2)->default(0);
             $table->float('total_for_items', 8, 2)->default(0);
             $table->float('total', 8, 2)->default(0);
             $table->timestamps();
---
+++ src/database/migrations/2022_10_17_151231_add_shipping_type_col_to_orders_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->foreignId('shipping_type_id')->constrained('shipping_types')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropForeign(['shipping_type_id']);
+            $table->dropColumn('shipping_type_id');
+        });
+    }
+};
