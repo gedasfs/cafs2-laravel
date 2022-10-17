b43a4dfd238a6acca48ccfc02c898ce0cccbc4e3
Gediminas <gedas.fs@gmail.com>
2022-10-17 21:34:36 +0300

    fixes (orders/index.blade.php) in route definition

--- src/resources/views/orders/index.blade.php
+++ src/resources/views/orders/index.blade.php
@@ -6,7 +6,7 @@
 <h2>Orders</h2>
 <div class="list-group">
     @foreach ($orders as $order)
-    <a href="{{ route('order.show', $order->id) }}" class="list-group-item list-group-item-action" aria-current="true">
+    <a href="{{ route('orders.show', $order->id) }}" class="list-group-item list-group-item-action" aria-current="true">
         <div class="d-flex w-100 justify-content-between">
             <h5 class="mb-1">{{ $order->number }}</h5>
             <small></small>
