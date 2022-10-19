ecbad3ede41619e7084e6a9416cccd67118183c6
Gediminas <gedas.fs@gmail.com>
2022-10-19 21:57:32 +0300

    added value preservation in form inputs

--- src/resources/views/products/partials/filter.blade.php
+++ src/resources/views/products/partials/filter.blade.php
@@ -3,22 +3,25 @@
         <select class="form-select" aria-label="category_id" name="category_id">
             <option value="0" selected>Select Category</option>
             @foreach ($categories as $category)
-                <option value="{{ $category->id }}">{{ $category->name }}</option>
+                <option value="{{ $category->id }}" {{ $category->id == request('category_id') ? 'selected' : '' }}>{{ $category->name }}</option>
             @endforeach
           </select>
     </div>
 
     <div class="input-group mb-3">
         <span class="input-group-text">Filter by price</span>
-        <input type="text" name="price_from" placeholder="from" aria-label="from" class="form-control">
-        <input type="text" name="price_to" placeholder="to" aria-label="to" class="form-control">
+        <input type="text" name="price_from" placeholder="from" aria-label="from" class="form-control" value="{{ request('price_from') ?? '' }}">
+        <input type="text" name="price_to" placeholder="to" aria-label="to" class="form-control" value="{{ request('price_to') ?? '' }}">
     </div>
 
     <div class="mb-3">
         <span class="">Order By</span>
         <select class="form-select" name="order_by" aria-label="Default select example">
             @foreach ($orderBy as $value => $name)
-                <option value="{{ $value }}">{{ $name }}</option>
+                <option
+                    {{ $value == request('order_by') ? 'selected' : '' }}
+                    value="{{ $value }}"
+                >{{ $name }}</option>
             @endforeach
           </select>
     </div>
